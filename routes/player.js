75e87f1d1621a815b638fd228f8f6ec96a8256c5
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-26 22:15:11 +0000

    Started player route & .gitignore file

--- routes/player.js
+++ routes/player.js
@@ -1,4 +1,17 @@
 const { Router } = require("express");
 const playerRouter = Router();
 
+// Importing db models
+const { Player, Team } = require("../models/");
+
+// Get a player given a name
+playerRouter.get("/:name", async (req, res) => {
+	try {
+		const player = await Player.findOne({ where: { name: req.params.name } });
+		res.send({ success: true, player });
+	} catch (err) {
+		res.send({ success: false, error: err.message });
+	}
+});
+
 module.exports = playerRouter;
