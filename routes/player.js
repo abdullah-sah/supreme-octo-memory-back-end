0a57b51463834ca295bf7648c3df576371e4d734
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-26 21:22:39 +0000

    Basic boilerplate for route files

---
+++ routes/player.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const playerRouter = Router();
+
+module.exports = playerRouter;
---
+++ routes/team.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const teamRouter = Router();
+
+module.exports = teamRouter;
