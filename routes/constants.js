bb83a8e95261b1136829b6bbe9292c187df8f1d9
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-27 22:57:26 +0000

    Added another utility + updated routes to use them

---
+++ routes/constants.js
@@ -0,0 +1,3 @@
+const API_KEY = "f52051be-6a3b-4929-992f-d77d7dab4fc5";
+
+module.exports = { API_KEY };
--- routes/team.js
+++ routes/team.js
@@ -3,8 +3,7 @@ const teamRouter = Router();
 const { Player, Team } = require("../models/");
 
 // utilities
-const isValidId = require("../utilities/isValidId");
-const throwError = require("../utilities/throwError");
+const { isValidId, isIdOrName, throwError } = require("../utilities/index");
 
 // Creates new team and returns it
 teamRouter.put("/:name", async (req, res) => {
@@ -20,18 +19,18 @@ teamRouter.put("/:name", async (req, res) => {
 teamRouter.get("/:identifier", async (req, res) => {
 	try {
 		// storing req.params.identifier as a number. if a string is passed as identifier, toNumber = null
-		const toNumber = Number(req.params.identifier);
+		const identifier = isIdOrName(req.params.identifier);
 
 		// grabbing number of teams (rows) in database model
 		const count = await Team.count();
 
 		// if number passed in > number of rows in table, return error
-		const valid = isValidId(toNumber, count);
+		const valid = isValidId(identifier, count);
 		if (!valid) throwError("That team don't exist mate");
 
-		// if toNumber is NOT null, findByPk(toNumber) --- else findOne where name = string passed in
-		const team = toNumber
-			? await Team.findByPk(toNumber)
+		// if identifier is NOT null => findByPk(identifier) --- else => findOne where name = string passed in
+		const team = identifier
+			? await Team.findByPk(identifier)
 			: await Team.findOne({ where: { name: req.params.identifier } });
 
 		// making sure we actually returned a team from the db model
@@ -47,17 +46,17 @@ teamRouter.get("/:identifier", async (req, res) => {
 teamRouter.delete("/:identifier", async (req, res) => {
 	try {
 		// storing req.params.identifier as a number => if is name not id, will be equal to null
-		const toNumber = Number(req.params.identifier);
+		const identifier = isIdOrName(req.params.identifier);
 
 		// grabbing number of teams in db model
 		const count = await Team.count();
 
 		// making sure id is valid (is not > 0 or < count)
-		const valid = isValidId(toNumber, count);
+		const valid = isValidId(identifier, count);
 		if (!valid) throwError("That team don't exist mate");
 
-		toNumber
-			? await Team.destroy({ where: { id: toNumber } })
+		identifier
+			? await Team.destroy({ where: { id: identifier } })
 			: await Team.destroy({ where: { name: req.params.identifier } });
 
 		res.send({ success: true });
